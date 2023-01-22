602d595e93471b13c90a6351cd76b2b3d50eebf2
Alberto Rossotto <alberto.rossotto@gmail.com>
2023-01-22 10:06:58 +0100

    add missing file

---
+++ run-checks.sh
@@ -0,0 +1,13 @@
+# run bandit
+
+# TODO: To remove the line below in future as redundant given the environment.yml file. For now, had to install 'bandit'
+#  here too as on Mac it is not recognised even if installed via the environment.yml file.
+pip3 install bandit
+
+bandit --ini .bandit -r > bandit.txt
+
+# run pylint
+pylint --extension-pkg-whitelist=pygame rc_car/*.py features/steps/*.py > pylint.txt
+
+# run coverage
+pytest --cov-report term-missing --cov=rc_car --cov-config=.coveragerc tests
