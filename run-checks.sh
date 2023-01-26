5ebff6147f859fa972523a742ba0860f1ad2c852
Marianne Manaog <manaog.mariannelyne@gmail.com>
2023-01-26 00:00:45 +0000

    Use safety for further security scan and include both bandit and safety into CI/CD workflow. Also update dependencies in the build to include all of them.

--- run-checks.sh
+++ run-checks.sh
@@ -1,13 +1,13 @@
-# run bandit
-
-# TODO: To remove the line below in future as redundant given the environment.yml file. For now, had to install 'bandit'
-#  here too as on Mac it is not recognised even if installed via the environment.yml file.
-pip3 install bandit
+# run 'bandit' to scan for security vulnerabilities (none identified on Jan 25th, 2023)
 
 bandit --ini .bandit -r > bandit.txt
 
-# run pylint
+# run complementary library ('safety') to scan for security vulnerabilities (11 identified on Jan 25th, 2023)
+
+safety check -o text > safety_sec_scan.txt
+
+# run 'pylint' for code quality checks
 pylint --extension-pkg-whitelist=pygame $(git ls-files '*.py') > pylint.txt
 
-# run coverage
+# run 'pytest' and 'pytest-cov' for reporting test coverage
 pytest --cov-report term-missing --cov=rc_car --cov-config=.coveragerc tests
